1616f57af0cd121b36c3b08d954834131e2e9118
emil <shivan@shivan.dk>
2025-12-16 20:02:05 -0500

    update

--- examples/react-demo/vite.config.ts
+++ examples/react-demo/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'static-shard': path.resolve(__dirname, '../../dist/client/index.js')
+    }
+  }
 })
